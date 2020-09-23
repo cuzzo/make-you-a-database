80b4b9b72da7d3bbcf4d11720505cdea35b71bbc
Cuzzo <yahn007@outlook.com>
2020-09-23 07:52:44 -0700

    Implement naive in-memory storage.

--- glade.rb
+++ glade.rb
@@ -1,5 +1,25 @@
 #! /usr/bin/env ruby
 
+class Table
+  attr_reader :data
+
+  def initialize()
+    @data = []
+  end
+
+  def <<(obj)
+    @data << obj
+  end
+
+  def [](idx)
+    @data[idx]
+  end
+
+  def count()
+    @data.count
+  end
+end
+
 def print_prompt()
   print("db > ")
 end
@@ -13,19 +33,48 @@ def parse_meta_command(cmd)
   when "exit"
     exit
   else
-    raise "Error"
+    raise StandardError.new(:SyntaxError)
   end
+end
+
+# serialize record to bytes.
+### figure out byte size / format by schema
+### standard OS page size = 4096 bytes (4kb)
+# store bytes in array.
+def execute_insert(record)
+  $table << record
+  $table.count
+end
+
+# move to position in memory.
+# read size_of_record bytes.
+# deserialize bytes to record.
+def execute_select(id)
+  $table[Integer(id)]
+rescue
+  raise StandardError.new(:SelectError)
 end
 
 def parse_statement(statement)
   tokens = statement.split(/\s+/)
   case tokens.first.downcase
   when "select"
-    puts "DO SELECT"
+    execute_select(tokens[1])
   when "insert"
-    puts "DO INSERT"
+    execute_insert(tokens[1..-1])
   else
-    raise "Error"
+    raise StandardError.new(:SyntaxError)
+  end
+end
+
+def handle_error(e, str)
+  case e.message.to_sym
+  when :SyntaxError
+    $stderr.puts("Unrecognized command '#{str}'.")
+  when :SelectError
+    $stderr.puts("Could not SELECT '#{str}'.")
+  else
+    $stderr.puts("System failure: '#{e.message}'.")
   end
 end
 
@@ -33,12 +82,15 @@ def parse(str)
   if str[0] == "."
     parse_meta_command(str[1..-1])
   else
-    parse_statement(str)
+    resp = parse_statement(str)
+    puts(resp)
   end
-rescue
-  puts("Unrecognized command '#{str}'.")
+rescue => e
+  handle_error(e, str)
 end
 
+
+$table = Table.new
 while true do
   print_prompt()
   input = read_input()
